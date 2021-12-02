e0e26ae5525f44d579b798e4387076f8e1862d62
feng1234 <563005903@qq.com>
2021-12-02 17:41:55 +0800

    vite测试

---
+++ assets/About.d9fb84a8.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.9595a8c2.js";import{o,c as r}from"./vendor.f38f62de.js";const c={};function t(n,a){return o(),r("h1",null,"hello about")}var _=e(c,[["render",t]]);export{_ as default};
